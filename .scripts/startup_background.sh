6155227ff8247b38df88d0612a0b6d1b1fbfb210
dungxibo123 <vtd.hcmus@gmail.com>
2021-07-01 00:29:53 +0700

    fix a little bit in startup and i3config file, some update on .local

--- .scripts/startup_background.sh
+++ .scripts/startup_background.sh
@@ -1,12 +1,19 @@
-local DARKNESS_BACKGROUND=./bg/pq1.png
-local BRIGHTNESS_BACKGROUND=./bg/pq2.png
-local bgr=""
-local time=`date +%H`
+#!/bin/bash
 
-if [[ $time -ge 6 && $time -lt 17  ]] 
+export DARKNESS_BACKGROUND=~/.scripts/bg/pq2.png
+
+export BRIGHTNESS_BACKGROUND=~/.scripts/bg/pq1.png
+export bgr=""
+export brightness=50%
+export time=`date +%H`
+
+if [[ $time -ge 6 && $time -lt 17  ]]; 
 then
 	bgr=$BRIGHTNESS_BACKGROUND	
+	brightness=80%
 else
 	bgr=$DARKNESS_BACKGROUND
+	brightness=30%
 fi
 feh --bg-fill $bgr
+brightnessctl s $brightness
